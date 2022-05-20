cd0e0178ed0698747922229448b37fbb9a71ec91
rt4bc <bochao.me@gmail.com>
2022-05-20 13:16:08 +0800

    simplify for test on fpga

--- src/main/scala/mylib/MyTopLevel.scala
+++ src/main/scala/mylib/MyTopLevel.scala
@@ -26,19 +26,12 @@ import scala.util.Random
 //Hardware definition
 class MyTopLevel extends Component {
   val io = new Bundle {
-    val cond0 = in  Bool()
-    val cond1 = in  Bool()
-    val flag  = out Bool()
-    val state = out UInt(8 bits)
-  }
-  val counter = Reg(UInt(8 bits)) init(0)
-
-  when(io.cond0){
-    counter := counter + 1
+    val state = out UInt (8 bits)
   }
 
+  val counter = Reg(UInt(8 bits)) init (0)
+  counter := counter + 1
   io.state := counter
-  io.flag  := (counter === 0) | io.cond1
 }
 
 //Generate the MyTopLevel's Verilog
@@ -48,16 +41,13 @@ object MyTopLevelVerilog {
   }
 }
 
-//Generate the MyTopLevel's VHDL
-object MyTopLevelVhdl {
-  def main(args: Array[String]) {
-    SpinalVhdl(new MyTopLevel)
-  }
-}
-
-
 //Define a custom SpinalHDL configuration with synchronous reset instead of the default asynchronous one. This configuration can be resued everywhere
-object MySpinalConfig extends SpinalConfig(defaultConfigForClockDomains = ClockDomainConfig(resetKind = SYNC))
+object MySpinalConfig
+    extends SpinalConfig(
+  targetDirectory="./hdl/src",
+      defaultConfigForClockDomains =
+        ClockDomainConfig(resetKind = ASYNC, resetActiveLevel = LOW)
+)
 
 //Generate the MyTopLevel's Verilog using the above custom configuration.
 object MyTopLevelVerilogWithCustomConfig {
